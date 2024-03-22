a6743edd8ae9ec5fbdd0d047adc85754ded64490
najjamcgee <136377802+najjamcgee@users.noreply.github.com>
2024-03-22 13:07:30 -0400

    Add files via upload

---
+++ Programming Exercise 1.4_NMcGee.py
@@ -0,0 +1,25 @@
+#Name: Najja McGee
+#Date: June 14th, 2023
+#Course: CIS 111
+#Secion: 01
+#Program Name: Programming Exercise 1.4
+
+#Algorithm:
+  #Step 1: prompt the user for a width value then press enter
+  #Step 2: prompt the user for a length value then press enter
+  #Step 3: multiply the two values inputed 
+  #Step 4: display the product which will be the area of the rectangle
+
+#prompt the user for a width value then press enter
+width = int(input("Enter a width: "))
+
+#prompt the user for a length value then press enter
+length = int(input("Enter a length: "))
+
+#since we are finding the area of a rectangle these valuses will be multiplied and have a product
+
+product = width * length
+
+#then you will display this product value which is the area
+
+print("The area of the rectangle is: ", product)
